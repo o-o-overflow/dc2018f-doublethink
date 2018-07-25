32f0778d52ccd9eb6d0c6d371c1a5c8715b15f3a
Yan <yans@yancomm.net>
2018-07-24 18:16:24 -0700

    updates

--- service/platforms/run.sh
+++ service/platforms/run.sh
@@ -16,20 +16,20 @@ then
 fi
 
 TMPDIR=$(mktemp -d)
-trap "echo Cleaning up...; rm -rf $TMPDIR" EXIT
+trap "echo [**] Cleaning up...; rm -rf $TMPDIR" EXIT
 cd $TMPDIR
 echo "$FLAG" > flag
 echo -n "$SHELLCODE" | base64 -d > shellcode
 
-echo "[*] Running $PLATFORM..."
-env -i - PLATDIR=$PLATDIR script -q result -c "$PLATDIR/run ./flag ./shellcode"
-echo "[*] $PLATFORM shellcode terminated. Checking results."
+echo "[**] Running $PLATFORM..."
+env -i - PLATDIR=$PLATDIR timeout -s9 10 script -q result -c "$PLATDIR/run ./flag ./shellcode"
+echo "[**] $PLATFORM shellcode terminated. Checking results."
 cat result | tr 'A-Z' 'a-z' > results.lower
 if grep -q ${FLAG,,} results.lower
 then
-	echo "FLAG GOTTEN!"
+	echo "[:)] FLAG GOTTEN!"
 	exit 0
 else
-	echo "NO FLAG!"
+	echo "[:(] NO FLAG!"
 	exit 1
 fi
